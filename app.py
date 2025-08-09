55cff641f6ab42977d46264e2feb312d49f4770e
sanjuwenbot2 <hubgit222@gmail.com>
2025-08-09 14:34:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '@HxBots'
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Koyeb's assigned port
-    app.run(host="0.0.0.0", port=port)
+    app.run()
