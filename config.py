62da5df0039de227779b4a6b778812d8c67b4e77
sanjuwenbot2 <hubgit222@gmail.com>
2025-08-09 11:58:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7960171243:AAGRWhwwbY0y71bZbJv8NQv3gWi69rnO16g")
+API_ID = int(os.environ.get("API_ID", "20342933"))
+API_HASH = os.environ.get("API_HASH", "9233e5deebe6abfc9ba297a9678851be")
+OWNER = int(os.environ.get("OWNER", "6803963354"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "@Renamerxc_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "ExxMaster")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002519121603"))
 
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
